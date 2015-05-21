9768923f2ca294973b1f329d07206cfa07d8c42a
Matt York <myork@redhat.com>
2015-05-21 13:08:37 +0100

    Added some stub logging code

--- bin/test.py
+++ bin/test.py
@@ -1,17 +1,32 @@
 
 ## Import the sat6 library
 from pyhammer.api import Sat6
+import logging
 
 __author__ = 'myork'
 
+loglevel = logging.INFO
+username = 'myork'
+password = 'redhat'
+https = False
+hostname = '192.168.100.3'
+
 def main():
 
+
+    logging.basicConfig(level=loglevel)
+    logger = logging.getLogger(__name__)
+
     # Yes these passowrds are left here deliberatly!
-    s = Sat6(hostname='192.168.100.3',
-             username='myork',
-             password='redhat',
-             https=False)
-    org_name = 'Redhat_Consultingss'
+    s = Sat6(hostname=hostname,
+             username= username,
+             password=password,
+             https=https)
+
+    logging.info("Username: %s" % username )
+
+
+    org_name = 'Redhat_Consulting'
 
     #print s.getContentViews(0)
     print s.getOrganizationByName(org_name)
